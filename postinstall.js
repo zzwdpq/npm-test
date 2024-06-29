4e5fef366faaea83dec79c8f7e89c3f5a1551cde
lifuqiang <lifuqiang@kuaishou.com>
2024-06-29 17:14:11 +0800

    feat: fix code

--- postinstall.js
+++ postinstall.js
@@ -1,1 +1,8 @@
 console.log("📢 Attention: Version 5 includes theme customization options!");
+
+console.log(`
+    ****<span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span>*******************************
+    *   WARNING: Theme Changes Ahead  *
+    ***********************************
+    Upgrading to this version may result in significant theme changes. Please review the changes thoroughly before proceeding.
+    `);
